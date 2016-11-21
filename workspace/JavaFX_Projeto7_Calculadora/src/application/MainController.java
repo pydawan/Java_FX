d025835cd0ce4a5c5318c1ebe2c8d50dbec83f8a
Rafael Carvalho <rapha.pse@outlook.com>
2016-11-21 21:38:09 -0200

    Calculadora javaFX

--- workspace/JavaFX_Projeto7_Calculadora/src/application/MainController.java
+++ workspace/JavaFX_Projeto7_Calculadora/src/application/MainController.java
@@ -8,44 +8,44 @@ import javafx.scene.control.Label;
 
 public class MainController {
 
-	@FXML
-	private Label result;
-	private long number1 = 0;
-	private String operador = "";
-	private boolean start = true;
-	private Model model = new Model();
+		@FXML
+		private Label result;
+		private long number1 = 0;
+		private String operador = "";
+		private boolean start = true;
+		private Model model = new Model();
 
-	@FXML
-	public void processNumber(ActionEvent eventNumber) {
-		if (start) {
-			result.setText("");
-			start = false;
-		}
-		String value = ((Button) eventNumber.getSource()).getText();
-		result.setText(result.getText() + value);
-	}
-
-	@FXML
-	public void processOperators(ActionEvent eventOperator) {
-
-		String value = ((Button) eventOperator.getSource()).getText();
-		if (!value.equals("=")) {
-			if (!operador.isEmpty()) {
-				return;
+		@FXML
+		public void processNumber(ActionEvent eventNumber) {
+			if (start) {
+				result.setText("");
+				start = false;
 			}
-			operador = value;
-			number1 = Long.parseLong(result.getText());
-			result.setText("");
-		} else {
-			if (operador.isEmpty()) {
-				return;
-			}
-			long number2 = Long.parseLong(result.getText());
-			float output = model.calculadora(number1, number2, operador);
-			result.setText(String.valueOf(output));
-			operador = "";
-			start = true;
+			String value = ((Button) eventNumber.getSource()).getText();
+			result.setText(result.getText() + value);
 		}
-	}
 
-}
+		@FXML
+		public void processOperators(ActionEvent eventOperator) {
+
+			String value = ((Button) eventOperator.getSource()).getText();
+			if (!value.equals("=")) {
+				if (!operador.isEmpty()) {
+					return;
+				}
+				operador = value;
+				number1 = Long.parseLong(result.getText());
+				result.setText("");
+			} else {
+				if (operador.isEmpty()) {
+					return;
+				}
+				long number2 = Long.parseLong(result.getText());
+				float output = model.calculadora(number1, number2, operador);
+				result.setText(String.valueOf(output));
+				operador = "";
+				start = true;
+			}
+		}
+
+	}
