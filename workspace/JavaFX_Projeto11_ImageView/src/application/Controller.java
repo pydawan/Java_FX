d4445b6479ef51ceed669845aea737837e5a7041
Rafael Carvalho <rapha.pse@outlook.com>
2016-11-24 20:02:31 -0200

    javaFX login e senha com imageView - imagens

--- workspace/JavaFX_Projeto11_ImageView/src/application/Controller.java
+++ workspace/JavaFX_Projeto11_ImageView/src/application/Controller.java
@@ -2,9 +2,13 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 	
@@ -17,11 +21,18 @@ public class Controller {
 	@FXML
 	private PasswordField senha;
 	
-	@FXML
-	public void entrar(ActionEvent evento){
+	
+	public void entrar(ActionEvent evento) throws Exception{
 		
-		if(txt.equals("rafael") && senha.equals("rafael")){
+		if(txt.getText().equals("rafael") && senha.getText().equals("rafael")){
 			result.setText("OK");
+			
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene sena = new Scene(root);
+			sena.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(sena);
+			primaryStage.show();
 		}else{
 			result.setText("SENHA E LOGIN ERRADOS");
 		}
--- workspace/JavaFX_Projeto11_ImageView/src/application/Main.java
+++ workspace/JavaFX_Projeto11_ImageView/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
