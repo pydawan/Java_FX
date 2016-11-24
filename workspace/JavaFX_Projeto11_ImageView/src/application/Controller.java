6731425bc5e6135684432c0997e2ba4e99e76d41
Rafael Carvalho <rapha.pse@outlook.com>
2016-11-24 19:24:51 -0200

    javaFX login e senha com imageView - imagens

---
+++ workspace/JavaFX_Projeto11_ImageView/src/application/Controller.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+	
+	@FXML
+	private Label result;
+	
+	@FXML
+	private TextField txt;
+	
+	@FXML
+	private PasswordField senha;
+	
+	
+	public void entrar(ActionEvent evento){
+		
+		if(txt.equals("rafael") && senha.equals("rafael")){
+			result.setText("OK");
+		}else{
+			result.setText("SENHA E LOGIN ERRADOS");
+		}
+	}
+
+}
