1f0c8cfcd6cbac2f84c8ed556a6bac995a5ae853
Rafael <rapha.pse@outlook.com>
2016-12-07 19:42:43 -0200

    Projeto JavaFX - Menu - MenuBar - menuItem e Separadores de Menu

---
+++ workspace/JavaFX_ProjetoMenus/src/application/MainController.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void ClouseMenu(ActionEvent evento){
+		Platform.exit();
+		System.exit(0);
+	}
+
+}
