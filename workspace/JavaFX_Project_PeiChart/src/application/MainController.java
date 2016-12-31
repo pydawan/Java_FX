936be217bb7de0adc26ebe739de4093f7a5a64b5
Rafael <rapha.pse@outlook.com>
2016-12-31 13:48:24 -0200

    ultimo projeto - JAVAFX PieChart

--- workspace/JavaFX_Project_PeiChart/src/application/MainController.java
+++ workspace/JavaFX_Project_PeiChart/src/application/MainController.java
@@ -1,15 +1,20 @@
 package application;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 	
 	@FXML PieChart pieChart;
+	@FXML Label label1;
 	
 	
 	public void btn1(ActionEvent event){
@@ -23,6 +28,21 @@ public class MainController {
 				new PieChart.Data("python", 80)				
 				);
 		pieChart.setData(list);
+		
+		for (final PieChart.Data data : pieChart.getData()){
+			
+			data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					label1.setText(String.valueOf(data.getPieValue(
+							)/(125.00 / 100)) + "%");
+					
+				}
+				
+				
+			});
+		}
 	}
 
 }
