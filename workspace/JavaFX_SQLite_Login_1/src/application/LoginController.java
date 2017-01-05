23b42ba43ebd6fb02c4cc0199351376c3cccbb7d
Rafael <rapha.pse@outlook.com>
2017-01-05 10:38:11 -0200

    JavaFX with SQLite - Login of User

--- workspace/JavaFX_SQLite_Login_1/src/application/LoginController.java
+++ workspace/JavaFX_SQLite_Login_1/src/application/LoginController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.chrono.IsoChronology;
@@ -7,10 +8,15 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable{
 	
@@ -43,10 +49,28 @@ public class LoginController implements Initializable{
 			if(loginModel.isLogin(txtUser.getText(), txtSenha.getText())){
 				isConnected.setText("usuário e senha - Corretor");
 				
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/application/User.fxml").openStream());
+				
+				UserController userController = (UserController)loader.getController();
+				userController.GetUser(txtUser.getText());
+				Scene scene = new Scene(root);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+				
+				
+				
+				
+				
 			}else{
 				isConnected.setText("Usuário ou senha Invalido");
 			}
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
