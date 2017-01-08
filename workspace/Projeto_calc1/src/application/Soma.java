6192ed55dddf1212ccdc8a67c0cdf128d6c2c2d0
Rafael <rapha.pse@outlook.com>
2017-01-08 21:27:52 -0200

    Rest

---
+++ workspace/Projeto_calc1/src/application/Soma.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Soma {
+	
+	public static Long somar(long num1, long num2){
+		
+		return num1 + num2;
+	}
+
+	
+}
