2219fb44228fc33301c6abf1db32cd000fb5924a
Rafael <rapha.pse@outlook.com>
2017-01-08 23:16:54 -0200

    Executando uma calculadora Simples

---
+++ workspace/InformacaoJavaFX/src/application/MainController.java
@@ -0,0 +1,28 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+public class MainController{
+	
+	@FXML
+	private Label lb;
+	@FXML
+	private TextField tx;
+	
+	
+	@FXML
+	public void generateRandom(ActionEvent event){
+	
+		//Random rand = new Random();
+		//int myrand = rand.nextInt(50) + 1;
+		lb.setText(tx.getText());  //mensagem que será apresentada no label no app
+		//System.out.println(Integer.toString(myrand));
+	
+	}
+	
+	
+
+}
