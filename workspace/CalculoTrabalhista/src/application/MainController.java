19cfa6cfe82f9757b252057f3ad7f3ae6a766827
Rafael <rapha.pse@outlook.com>
2017-01-10 21:19:22 -0200

    New Calculadora trabalhista

---
+++ workspace/CalculoTrabalhista/src/application/MainController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+	
+	@FXML
+	private TextField tDias;
+	
+	public void informar(ActionEvent evento){
+		
+	}
+	
+	
+
+}
