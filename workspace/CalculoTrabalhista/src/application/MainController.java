a4112caeda0382be8c007afab86ce85b295708f9
Rafael <rapha.pse@outlook.com>
2017-01-10 23:29:09 -0200

    New Calculadora trabalhista

---
+++ workspace/CalculoTrabalhista/src/application/MainController.java
@@ -0,0 +1,99 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.Label;
+
+public class MainController {
+	@FXML
+	private Button btnCalc;
+	@FXML
+	private TextField lbDias;
+	@FXML
+	private Button btnRecalc;
+	@FXML
+	private Button btnSair;
+	@FXML
+	private Label li;
+	@FXML
+	private Label piso;
+	@FXML
+	private Label refPiso;
+	@FXML
+	private Label refAdic;
+	@FXML
+	private Label refHoras;
+	@FXML
+	private Label refHorasSeis;
+	@FXML
+	private Label refHorasCem;
+	@FXML
+	private Label bruto;
+	@FXML
+	private Label vPiso;
+	@FXML
+	private Label vAdc;
+	@FXML
+	private Label vHoras;
+	@FXML
+	private Label vHorasSeis;
+	@FXML
+	private Label vHorasCem;
+	@FXML
+	private Label vDSR;
+	@FXML
+	private Label adc;
+	@FXML
+	private Label hT;
+	@FXML
+	private Label horasSeis;
+	@FXML
+	private Label horasCem;
+	@FXML
+	private Label dsr;
+	@FXML
+	private Label ld;
+	@FXML
+	private Label inss;
+	@FXML
+	private Label vTrans;
+	@FXML
+	private Label vRef;
+	@FXML
+	private Label conv;
+	@FXML
+	private Label sind;
+	@FXML
+	private Label desconto;
+	@FXML
+	private Label vDesconto;
+	@FXML
+	private Label refSind;
+	@FXML
+	private Label refCOnv;
+	@FXML
+	private Label refVR;
+	@FXML
+	private Label refVT;
+	@FXML
+	private Label vSind;
+	@FXML
+	private Label vConv;
+	@FXML
+	private Label vVR;
+	@FXML
+	private Label vVT;
+	@FXML
+	private Label vInss;
+	@FXML
+	private Label refInss;
+	@FXML
+	private Label liquido;
+	@FXML
+	private TextField lbFDS;
+
+}
