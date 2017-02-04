dcee594b7e943dcf9309d234a10fb65123286c36
Rafael <rapha.pse@outlook.com>
2017-02-04 14:06:12 -0200

    Uso do comboBox com textfield para inserir dados

--- ComboBox/src/application/MainController.java
+++ ComboBox/src/application/MainController.java
@@ -12,30 +12,28 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class MainController implements Initializable{
-	
-	
+public class MainController implements Initializable {
+
 	@FXML
 	public ComboBox<String> combobox;
 	@FXML
 	private Label myLabel;
 	@FXML
 	private TextField txt;
-	
-	
+
 	ObservableList<String> lista = FXCollections.observableArrayList();
-		
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		combobox.setItems(lista);
-		
+
 	}
-	public void combobox(ActionEvent actionEvent){
-		
+
+	public void combobox(ActionEvent actionEvent) {
+
 		lista.add(txt.getText());
-		//myLabel.setText(combobox.getValue());
+		// myLabel.setText(combobox.getValue());
 	}
 
 }
