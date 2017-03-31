137e57fa608deb6935acdd4154b168456f21ce3e
Rafael <rapha.pse@outlook.com>
2017-03-31 13:05:19 -0300

    Adicionando

--- Quiz/src/application/MainController.java
+++ Quiz/src/application/MainController.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -72,6 +81,18 @@ public class MainController {
 		}
 
 		informar();
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Segunda.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Stage secundaryStage = new Stage();
+			secundaryStage.setScene(scene);
+			secundaryStage.show();	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 
 	}
 
@@ -110,10 +131,14 @@ public class MainController {
 		if (this.mais == 0) {
 			lbResultado.setText("Zero Acerto ");
 		}
+		
+		
 	}
+
 	
 	public void sair(ActionEvent event){
 		System.exit(0);
 	}
+
 
 }
